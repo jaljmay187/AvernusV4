46c8ce50a3e01cca95811d97ad135582b54c8429
GL YT MX <148560941+glytglobal@users.noreply.github.com>
2024-06-22 18:20:57 -0300

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,5 +1,4 @@
 var handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let pp = gataImg        
 const smsowner = `*---------------------*
 
@@ -18,7 +17,7 @@ _GRACIAS POR USAR AVERNUSBOT-MD SI NECESITAS AYUDA O ASISTENCIA EN LA INSTALACIO
 
 *---------------------*`
 
-await conn.reply(m.chat, pp, smsowner, fkontak)
+await conn.reply(m.chat, pp, smsowner, fakeChannel)
 }
 handler.help = ['creadorofc']
 handler.tags = ['glytmx']
