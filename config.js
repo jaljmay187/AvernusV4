bd6724c58f328ace9fffd7c3e1e86e7f3ee49f80
GL YT MX <148560941+glytglobal@users.noreply.github.com>
2024-06-23 15:34:07 -0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import fetch from "node-fetch"
 import axios from "axios"
 import moment from "moment-timezone"
 import { es as esDefault, en as enDefault } from "./lib/multi-language/_default.js"
-import { en, es, id, ar, pt } from "./lib/idiomas/total-idiomas.js"
+import { en, es } from "./lib/idiomas/total-idiomas.js"
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 // [ES] > Agrega el número que será Bot y los que serán propietarios.
