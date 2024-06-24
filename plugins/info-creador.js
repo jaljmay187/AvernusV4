ee8da2948c403638e1b78206674d6d503bd66d13
GL YT MX <148560941+glytglobal@users.noreply.github.com>
2024-06-24 20:16:18 -0300

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,4 +1,17 @@
-const handler = async (m, {conn, usedPrefix}) => {
+let handler  = async (m, { conn, usedPrefix, command }) => {
+let picture = './media/menus/Menu1.jpg'
+let gata = `𝙄𝙉𝙎𝙏𝘼𝙇𝘼𝘾𝙄𝙊𝙉 𝘿𝙀 AvernusBot (⁠◠⁠‿⁠◕⁠)`
+await 
+//conn.sendFile(m.chat, gataImg, 'lp.jpg', gata, fkontak, false, { contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: gt, body: ' 🌹 AvernusBot 💥 ', previewType: 0, thumbnail: imagen4, sourceUrl: accountsgb }}})}
+conn.sendButton(m.chat, gata, `*Proximamente AvernusBot💥*\n\n${ig}\n${wm}`, picture, [
+['𝘾𝙪𝙚𝙣𝙩𝙖𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 | 𝘼𝙘𝙘𝙤𝙪𝙣𝙩𝙨 ✅', '.cuentasgb'],
+['🎁 𝘿𝙤𝙣𝙖𝙧 | 𝘿𝙤𝙣𝙖𝙩𝙚', '.donar']], 'termux-setup-storage\n\napt update -y && yes | apt upgrade && pkg install -y bash wget mpv && wget -O - https://raw.githubusercontent.com/GataNina-Li/GataBot-MD/master/gata.sh | bash', fkontak)}
+handler.command = /^(instalarbot|instalargatabot|instalargata|procesobot|botproceso|procesodelbot|botinstall|installbot)/i
+export default handler
+
+
+
+/* const handler = async (m, {conn, usedPrefix}) => {
   const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
   const document = doc[Math.floor(Math.random() * doc.length)];
   const text = `*_GRACIAS POR USAR ESTE BOT_*\n\n• Creador: Gabriel Ofc\n• Bot: AvernusBot-MD\n• Tipo: WhatsAppBot\n• Libreria: Baileys\n• Entorno: NodeJS\n\n> SOPORTE : SUPPORT\n\n◆ *ASISTENCIA : TELEGRAM\n> t.me/Avernus_SupportBot\n◆ *ASISTENCIA : CORREO*\n> *avernusbotwa@gmail.com*\n\n*------------------*\n\n◆ ıllıllı ＡＶＥＲＮＵＳ － ＢＯＴ ıllıllı\n\n*---------------------*``.trim();
