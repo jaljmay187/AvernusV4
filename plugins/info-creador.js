69162eca1ff17a5b76e848d7a64ce59499b20727
GL YT MX <148560941+glytglobal@users.noreply.github.com>
2024-06-24 18:59:45 -0300

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,4 +1,35 @@
-var handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
+const handler = async (m, {conn, usedPrefix}) => {
+  const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
+  const document = doc[Math.floor(Math.random() * doc.length)];
+  const text = `*_GRACIAS POR USAR ESTE BOT_*\n\n• Creador: Gabriel Ofc\n• Bot: AvernusBot-MD\n• Tipo: WhatsAppBot\n• Libreria: Baileys\n• Entorno: NodeJS\n\n> SOPORTE : SUPPORT\n\n◆ *ASISTENCIA : TELEGRAM\n> t.me/Avernus_SupportBot\n◆ *ASISTENCIA : CORREO*\n> *avernusbotwa@gmail.com*\n\n*------------------*\n\n◆ ıllıllı ＡＶＥＲＮＵＳ － ＢＯＴ ıllıllı\n\n*---------------------*``.trim();
+  const buttonMessage= {
+    'document': {url: `https://whatsapp.com/channel/0029VaAN15BJP21BYCJ3tH04`},
+    'mimetype': `application/${document}`,
+    'fileName': `「${mdk}」`,
+    'fileLength': 99999999999999,
+    'pageCount': 600000,
+    'contextInfo': {
+      'forwardingScore': 200,
+      'isForwarded': true,
+      'externalAdReply': {
+        'mediaUrl': 'https://whatsapp.com/channel/0029VaiYqsM90x2u3k4WuA29',
+        'mediaType': 2,
+        'previewType': 'pdf',
+        'title': 'NUMEROS OFICIALES',
+        'body': wm,
+        'thumbnail': imagen1,
+        'sourceUrl': 'https://chat.whatsapp.com/Lb1iaTkpIip7aMG6PEuIJ9'}},
+    'caption': text,
+    'footer': wm,
+    'headerType': 6};
+  conn.sendMessage(m.chat, buttonMessage, {quoted: m});
+};
+handler.help = ['owner', 'creator'];
+handler.tags = ['info'];
+handler.command = /^(owner|creator|creador|propietario)$/i;
+export default handler;
+
+/*var handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 let pp = gataImg        
 const smsowner = `*---------------------*
 
